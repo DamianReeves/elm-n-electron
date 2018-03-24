b8dd16228012bd124774f2138de3439835b8429d
Damian Reeves <damreev@gmail.com>
2018-03-24 11:32:08 -0400

    Elm and electron, initial version

---
+++ ports.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 6 */
+/* eslint-env es6, node, browser */
+
+'use strict';
+const Elm = require('./elm.js');
+
+// get a reference to the div where we will show our UI
+let container = document.getElementById('container');
+
+// start the elm app in the container
+// and keep a reference for communicating with the app
+let feldspar = Elm.Main.embed(container);
